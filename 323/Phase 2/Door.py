b0039cfd6cced064d7e37712758cadbaad3d5fd3
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-18 11:35:51 -0800

    Update Door.py
    the unknown part is the junction table that connects the relationship between hook and door class.

--- 323/Phase 2/Door.py
+++ 323/Phase 2/Door.py
@@ -1,3 +1,4 @@
+'''
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from orm_base import Base
@@ -8,3 +9,30 @@ class Door(Base):
     
     def __ini__(self, doorType: String):
         self.doorType = doorType
+'''
+from sqlalchemy import Column, Integer, Identity, Float, \
+    String, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from orm_base import Base
+from Unknown import Unknown
+
+class Door(Base):
+    __tablebname__ = "doors"
+    door_type = Column('door_type', String(100), nullable=False)
+    hooks_list : [Unknown] = relationship("Unknown", back_populates="door", viewonly=False)
+    def __init__(self, door_type: String):
+        self.door_type = door_type
+        self.hooks_list = []
+
+    def add_hook(self, hook):
+        for next_hook in self.hooks_list:
+            if next_hook == hook:
+                return
+        # Create an instance of the junction table class.
+        unknown = Unknown(hook, self)
+        # add that new instance to the list of genres that the Movie keeps.
+        hook.doors_list.append(unknown)
+        # add that new instance to the list of movies that this genre keeps.
+        self.hooks_list.append(unknown)
+
