a477f58f56e4eb9a6a2166250143560727165d6c
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-18 15:16:15 -0800

    Update Employee.py

--- 323/Phase 2/Employee.py
+++ 323/Phase 2/Employee.py
@@ -5,7 +5,8 @@ from orm_base import Base
 class Emplyoee(Base):
     __tablename__ = "employee"
     name = Column("name", String(40), nullable=False)
-    emplyoeeID = Column("employee_id", Integer, nullable=False)
+    emplyoeeID = Column('employee_id', Integer, Identity(start=1, cycle=True),
+                       nullable=False, primary_key=True)
     
 def _init_(self, name: String, employeeID: Integer):
     self.name = name
