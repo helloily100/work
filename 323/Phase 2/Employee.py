a3ff729acf202de8055030cf39dab750ad7bb8bb
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-18 20:48:01 -0800

    change employee id stuff

--- 323/Phase 2/Employee.py
+++ 323/Phase 2/Employee.py
@@ -5,8 +5,8 @@ from orm_base import Base
 class Emplyoee(Base):
     __tablename__ = "employee"
     name = Column("name", String(40), nullable=False)
-    emplyoeeID = Column("employee_id", Integer, nullable=False)
-    
-def _init_(self, name: String, employeeID: Integer):
+    employee_id = Column('employee_id', Integer, Identity(start=1, cycle=True),
+                       nullable=False, primary_key=True)
+    key_request = relationship("KeyRequest")
+def _init_(self, name: String):
     self.name = name
-    self.employeeID = employeeID 
