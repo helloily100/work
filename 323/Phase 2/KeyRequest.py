9a099b3072533a5ec2b478ad8580c59b46cb8c4c
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-18 21:26:57 -0800

    Update KeyRequest.py

--- 323/Phase 2/KeyRequest.py
+++ 323/Phase 2/KeyRequest.py
@@ -4,10 +4,12 @@ from orm_base import Base
 
 class KeyRequest(Base):
     __tablename__ = "key_request"
+    employee_id = Column(Integer, ForeignKey("employees.employee_id"), primary_key= True, nullable= False)
     room = Column("room", Integer, nullable=False)
     requestDate = Column("request_date", Date, nullable=False)
     
-    def __init__ (self, room: Integer, requestDate: Date):
+    def __init__ (self, employee, room: Integer, requestDate: Date):
+        self.employee_id = employee.employee_id
         self.room = room
         self.requestDate = requestDate
         
