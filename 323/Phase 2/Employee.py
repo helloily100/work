25d56591a9f283955f3ee6326054d25401d6f6bc
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-18 20:38:39 -0800

    Update Employee.py

--- 323/Phase 2/Employee.py
+++ 323/Phase 2/Employee.py
@@ -5,8 +5,10 @@ from orm_base import Base
 class Emplyoee(Base):
     __tablename__ = "employee"
     name = Column("name", String(40), nullable=False)
-    emplyoeeID = Column("employee_id", Integer, nullable=False)
+    emplyoeeID = Column('employee_id', Integer, Identity(start=1, cycle=True),
+                       nullable=False, primary_key=True)
     
-def _init_(self, name: String, employeeID: Integer):
+def _init_(self, name: String):
     self.name = name
-    self.employeeID = employeeID 
+    #don't really need it, it will still include the id anyway (at least it worked for me on the other sql hw)
+    #self.employeeID = employeeID 
